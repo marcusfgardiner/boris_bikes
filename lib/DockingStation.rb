7066e2a906b403196fa22c1115e4ed00f6931140
Stephen Tilley <stephentilley@gmail.com>
2018-01-23 16:17:48 +0000

    Created an instance variable of bikes in the DockingStation class

--- lib/DockingStation.rb
+++ lib/DockingStation.rb
@@ -1,7 +1,17 @@
 require_relative "Bike"
 
 class DockingStation
+
+  attr_reader :bikes
+
+  def initialize
+    @bikes = []
+  end
+
   def release_bike
     Bike.new
+  end
+
+  def dock
   end
 end
--- spec/DockingStation_spec.rb
+++ spec/DockingStation_spec.rb
@@ -4,6 +4,8 @@ require "Bike"
 describe DockingStation do
   it {is_expected.to respond_to(:release_bike)}
 
+  it {is_expected.to respond_to(:dock)}
+
   it "release_bike creates a new bike object" do
     expect(subject.release_bike).to be_instance_of(Bike)
   end
@@ -11,5 +13,9 @@ describe DockingStation do
   it "checks the bike is working" do
     expect(subject.release_bike).to be_working
   end
+
+  # it "Allow a a bike to be docked" do
+  #   expect(subject.release_bike).to respond_to(:dock)
+  # end
 
 end
