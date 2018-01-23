a432b32cded22eda1f5147932cbb4552ac27a695
Stephen Tilley <stephentilley@gmail.com>
2018-01-23 14:20:45 +0000

    Added and tested release_bike method

--- spec/DockingStation_spec.rb
+++ spec/DockingStation_spec.rb
@@ -1,7 +1,7 @@
 require "DockingStation.rb"
 
 describe DockingStation do
-  # it "???" do
-  #   expect().to eq()
-  # end
+  it "responds to release_bike method" do
+    expect(docking_station = DockingStation.new).respond_to(release_bike)
+  end
 end
