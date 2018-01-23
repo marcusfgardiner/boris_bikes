c1df9fda4491aa2ac1a0dccab2f28791a15c3084
marcusfgardiner <marcusfgardiner@gmail.com>
2018-01-23 15:56:07 +0000

    Bike methods now woring - release bike and working

--- spec/DockingStation_spec.rb
+++ spec/DockingStation_spec.rb
@@ -1,9 +1,15 @@
 require "DockingStation"
+require "Bike"
 
 describe DockingStation do
   it {is_expected.to respond_to(:release_bike)}
 
   it "release_bike creates a new bike object" do
-    expect(release_bike).to eq()
+    expect(subject.release_bike).to be_instance_of(Bike)
   end
+
+  it "checks the bike is working" do
+    expect(subject.release_bike).to be_working
+  end
+
 end
